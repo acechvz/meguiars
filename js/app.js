75ba95ca021b0b507d59503123ec605943a5dc06
Alan <acechvz@gmail.com>
2017-10-13 03:25:49 -0500

    - Punto al final de parrafo

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
     console.log('Mailchimp script');
 
-    $('footer .container .row').append('<div class="col-sm-4 custom_footer custom_footer5"><h4>Suscribite al newsletter</h4><ul><li>Suscribite a nuestro newsletter para enterarte de las novedades</li></ul><div id="newsletter-form"></div></div>');
+    $('footer .container .row').append('<div class="col-sm-4 custom_footer custom_footer5"><h4>Suscribite al newsletter</h4><ul><li>Suscribite a nuestro newsletter para enterarte de las novedades.</li></ul><div id="newsletter-form"></div></div>');
 
     $('#newsletter-form').load('https://alanchavez09.github.io/meguiars/js/mailchimp.html');
 });
